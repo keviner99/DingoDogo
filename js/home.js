17258edf6ebb439804db650dc840fcdfe9fa3021
KEVIN ESPINOZA <robert.kevin.espinoza@gmail.com>
2022-12-08 13:53:32 -0500

    changes

---
+++ js/home.js
@@ -0,0 +1,6 @@
+const hamButton = document.querySelector('.ham');
+const mainMenu = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => { mainMenu.classList.toggle('responsive') }, false);
+
+window.onresize = () => { if (window.innerWidth > 760) mainMenu.classList.remove('responsive') };
